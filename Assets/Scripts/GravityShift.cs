de4ea1db0a85fdda05987d9ff4b8364da23208c1
Adrian Nawrocki <anawrocki@edu.cdv.pl>
2025-02-08 20:31:47 +0100

    JEST KURWA!!!!

--- Assets/Scripts/GravityShift.cs
+++ Assets/Scripts/GravityShift.cs
@@ -3,25 +3,17 @@ using UnityEngine;
 
 public class GravityShift : MonoBehaviour
 {
-    public Transform parent; // Obiekt, który ma byæ obracany
-    public float rotationSpeed = 90f; // Szybkoœæ obrotu w stopniach na sekundê
+    public Transform parent; 
+    public float rotationSpeed = 90f; 
     private bool isRotating = false;
     private bool playerInZone = false;
-    public Rigidbody playerRb;
-    
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !isRotating)
         {
             playerInZone = true;
-            playerRb = other.GetComponent<Rigidbody>();
-
-            if (playerRb != null)
-            {
-                playerRb.useGravity = false; // Wy³¹cz grawitacjê gracza
-                playerRb.linearVelocity = Vector3.zero; // Zatrzymaj ruch gracza
-            }
 
             StartCoroutine(RotateParent());
         }
@@ -45,17 +37,12 @@ public class GravityShift : MonoBehaviour
             float step = rotationSpeed * Time.deltaTime;
             if (rotatedAmount + step > 90f)
             {
-                step = 90f - rotatedAmount; // Dok³adnie dokoñcz obrót do 90 stopni
+                step = 90f - rotatedAmount; 
             }
 
             parent.Rotate(0, 0, step);
             rotatedAmount += step;
             yield return null;
-        }
-
-        if (playerRb != null)
-        {
-            playerRb.useGravity = true; // Przywróæ grawitacjê gracza
         }
 
         isRotating = false;
