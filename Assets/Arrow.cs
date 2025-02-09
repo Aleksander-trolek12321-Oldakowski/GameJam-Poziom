d6fef76ae0d7d16ccba1a71602dbf46f46a54325
Adrian Nawrocki <anawrocki@edu.cdv.pl>
2025-02-09 09:28:10 +0100

    Strzała

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-
-    public class Arrow : MonoBehaviour
-    {
+public class Arrow : MonoBehaviour
+{
     public GameObject arrow;
-        [SerializeField] private float damage = 2f;
-        void Start()
-        {
-            Destroy(gameObject, 3f);
-        }
+    [SerializeField] private float damage = 2f;
+    void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
 
-        private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             Destroy(gameObject);
         }
     }
+}
